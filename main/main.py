aca9bd6a541f5897c3a76a3bddba3ba1225f2c36
Sebastián Segura <133933619+ssebastiansegura@users.noreply.github.com>
2026-05-20 12:30:04 -0600

    genai test one

---
+++ main/main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"I'ts always sunny in {city}!"
+
+GENAI_MODEL = "gemini-2.5-flash-lite"
+GENAI_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model=GENAI_MODEL, 
+    google_api_key=GENAI_API_KEY
+)
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant.",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content)
